e010eeffb41d866d33106ace6be6025011603b6c
berryboylb <phemmynesce4life@gmail.com>
2024-02-17 04:18:58 +0100

    @chore: loan

--- loan.sol
+++ loan.sol
@@ -1,33 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity >=0.6.0 <0.8.0;
+pragma abicoder v2;
+
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Loan {
     using SafeMath for uint256;
     address payable owner;
 
-    //history struct
-    struct History {
-        uint256 amount;
-        uint256 timestamp;
-    }
     //user struct
     struct User {
         string name;
         uint256 currently_owed;
         uint256 total_borrowed;
         uint8 tier;
-        History[] history;
+        uint256 timestamp;
     }
+    //tier limits
+    uint256[3] public tierLimits = [5 ether, 10 ether, 15 ether];
+
+    //mapping to track user existence
+    mapping(address => bool) private userExists;
 
     //mapping to store user
     mapping(address => User) Accounts;
-    //mapping to track user existence
-    mapping(address => bool) private userExists;
-    //tier limits
-    uint256[3] public tierLimits = [1 ether, 2 ether, 3 ether];
 
-    modifier userExists() {
+    modifier userExist() {
         require(userExists[msg.sender], "User does not exist");
         _;
     }
@@ -44,19 +42,19 @@ contract Loan {
     event MoneyPaidBack(address indexed userAddress, uint256 amount);
 
     //assigning the owner of this contract on creation
-    constructor() {
+    constructor() payable {
         owner = payable(msg.sender);
     }
 
     //create a new user
-    function createUser(string name) public userDoesNotExist {
+    function createUser(string memory name) public userDoesNotExist {
         //create a new user
         User memory newUser = User({
             name: name,
             currently_owed: 0,
             total_borrowed: 0,
             tier: 1,
-            history: new History[](0)
+            timestamp: 0
         });
         // add to map
         Accounts[msg.sender] = newUser;
@@ -66,14 +64,15 @@ contract Loan {
         emit UserCreated(msg.sender, newUser);
     }
 
-    function getUser() public view userExists returns (User memory) {
+    function getUser() public view userExist returns (User memory) {
         //returns user
         return Accounts[msg.sender];
     }
 
-    function deleteAccount() public userExists {
+    function deleteAccount() public userExist {
         //fetch user
         User storage user = Accounts[msg.sender];
+        require(user.currently_owed == 0, "Please payback your previous loan");
         //delete from mapping
         delete Accounts[msg.sender];
         // remove from exists
@@ -82,7 +81,11 @@ contract Loan {
         emit UserDeleted(msg.sender, user);
     }
 
-    function borrowMoney(uint256 amount) public userExists {
+    function calculateInterest(uint256 amount) public pure returns (uint256) {
+        return amount.mul(20).div(100); //  20% interest
+    }
+
+    function borrowMoney(uint256 amount) public userExist {
         require(amount > 0, "Cannot borrow zero or negative amounts");
         User storage user = Accounts[msg.sender];
         require(user.currently_owed == 0, "Please payback your previous loan");
@@ -95,22 +98,17 @@ contract Loan {
         );
 
         // Transfer money from the contract to the user
-        (bool sent, bytes memory data) = payable(address(this)).call{
-            value: amount
-        }("");
+        (bool sent, ) = payable(address(this)).call{value: amount}("");
         require(sent, "Failed to send Loan");
 
         // Update user's balance
         user.currently_owed = amount;
+        user.timestamp = block.timestamp + 24 hours;
 
         emit MoneyBorrowed(msg.sender, amount);
     }
 
-    function calculateInterest(uint256 amount) public pure returns (uint256) {
-        return amount.mul(20).div(100); //  20% interest
-    }
-
-    function PaybackMoney(uint256 amount) public userExists {
+    function PaybackMoney(uint256 amount) public userExist {
         User storage user = Accounts[msg.sender];
         uint256 amountWithInterest = user.currently_owed.add(
             calculateInterest(user.currently_owed)
@@ -119,16 +117,20 @@ contract Loan {
         //check for currently owed and interest
         require(user.currently_owed > 0, "You are not owing");
         require(amount >= amountWithInterest, "Please add the 2% interest");
+        require(
+            block.timestamp >= user.timestamp,
+            "It's not yet your payback date"
+        );
 
         // Transfer money from the user to the contract
-        (bool sent, bytes memory data) = msg.sender.call{value: amount}("");
+        (bool sent, ) = msg.sender.call{value: amount}("");
         require(sent, "Failed to payback loan");
 
         // Calculate new tier before updating balances
         uint8 newTier = user.tier;
-        if (user.total_borrowed.add(user.currently_owed) > 20) {
+        if (user.total_borrowed.add(user.currently_owed) > 50) {
             newTier = 3;
-        } else if (user.total_borrowed.add(user.currently_owed) > 10) {
+        } else if (user.total_borrowed.add(user.currently_owed) > 30) {
             newTier = 2;
         }
 
@@ -136,15 +138,14 @@ contract Loan {
         user.total_borrowed = user.total_borrowed.add(user.currently_owed);
         user.currently_owed = 0;
         user.tier = newTier;
-
-        // Add to history
-        History memory newEntry = History({
-            amount: amount,
-            timestamp: block.timestamp
-        });
-        user.history.push(newEntry);
+        user.timestamp = 0;
 
         // Emit event
         emit MoneyPaidBack(msg.sender, amount);
     }
+
+    fallback() external payable {}
+
+    // Receive function
+    receive() external payable {}
 }
